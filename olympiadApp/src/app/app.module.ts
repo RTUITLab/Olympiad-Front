80784163707db6f09824bce47fd2463077a93468
Maksim <maksalmak@gmail.com>
2018-01-24 21:29:40 +0300

    Base markup, first route

--- olympiadApp/src/app/app.module.ts
+++ olympiadApp/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
+const routes = [
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- olympiadApp/src/app/user-state.service.ts
+++ olympiadApp/src/app/user-state.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { User } from '../models/User';
 @Injectable()
 export class UserStateService {
 
   constructor() { }
+
+  private _logs = new BehaviorSubject<User>(undefined);
+  public currentUser = this._logs.asObservable();
 
 }
