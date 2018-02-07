e28245bd31d6918aaf481e4cc6fbe253063096f9
Maksim <maksalmak@gmail.com>
2018-02-07 20:17:52 +0300

    adding overview text

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class OverviewComponent implements OnInit {
 
   constructor() { }
-
+  someCode = `function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello there!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }`;
   ngOnInit() {
   }
 
