42e6f79c62c94f136acf29decd31a2fba7f7681b
Maksim <maksalmak@gmail.com>
2018-05-19 21:12:14 +0300

    use remote backend

--- src/app/services/EndPoints.ts
+++ src/app/services/EndPoints.ts
@@ -1,6 +1,6 @@
 export class EndPoints {
-    protected readonly ip = 'http://localhost';
-    // protected readonly ip = 'https://ittestsback.azurewebsites.net';
-    protected readonly port = 5000;
-    // protected readonly port = 443;
+    // protected readonly ip = 'http://localhost';
+    protected readonly ip = 'https://ittestsback.azurewebsites.net';
+    // protected readonly port = 5000;
+    protected readonly port = 443;
 }
