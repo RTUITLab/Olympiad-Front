b1a306daddc1cc06c63f2d82d3355f0682f61f60
ReC <kitanin.ser@mail.ru>
2019-02-14 22:42:42 +0300

    added valid error messages

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { LoadingComponent } from '../helpers/loading-component';
 import { ToastrService } from 'ngx-toastr';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormValidateService } from 'src/app/services/FormHelpers/form-validate.service';
 
 
 @Component({
@@ -16,11 +17,11 @@ export class LoginComponent extends LoadingComponent implements OnInit {
   errorMessage: string;
   model: LoginViewModel;
   loginForm = new FormGroup({
-    Email: new FormControl('',[
+    Email: new FormControl('', [
       Validators.email,
       Validators.required
     ]),
-    Password: new FormControl('',[
+    Password: new FormControl('', [
       Validators.required,
       Validators.minLength(6)
     ])
@@ -29,27 +30,32 @@ export class LoginComponent extends LoadingComponent implements OnInit {
     private userState: UserStateService,
      private router: Router,
      private toastr: ToastrService,
+     private formValid: FormValidateService
      ) {
     super();
   }
 
   onSubmit() {
-    this.model = this.loginForm.value;
-    console.log(this.model);
-    this.startLoading();
-    this.userState.Login(this.model).subscribe(
-      success => {
-        this.router.navigate(['overview']);
-        this.stopLoading();
+    if (this.loginForm.valid) {
+      this.model = this.loginForm.value;
+      console.log(this.loginForm);
+      this.startLoading();
+      this.userState.Login(this.model).subscribe(
+        success => {
+          this.router.navigate(['overview']);
+          this.stopLoading();
 
-      },
-      error => {
-        this.errorMessage = error;
-        this.stopLoading();
-        this.toastr.error(error, `Ошибка`);
+        },
+        error => {
+          this.errorMessage = error;
+          this.stopLoading();
+          this.toastr.error(error, `Ошибка`);
 
-      }
-    );
+        }
+      );
+    } else {
+      this.formValid.validateAllFormFields(this.loginForm);
+    }
   }
   ngOnInit() {
   }
