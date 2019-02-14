16780d5280ecd2e295e543eebecece799f7f0239
ReC <kitanin.ser@mail.ru>
2019-02-14 22:44:38 +0300

    changed model of registration

--- src/app/models/ViewModels/RegisterViewModel.ts
+++ src/app/models/ViewModels/RegisterViewModel.ts
@@ -1,12 +1,13 @@
 export class RegisterViewModel {
     constructor(
+        public FirstName: string,
+        public LastName: string,
         public Email: string,
         public Password: string,
-        public FirstName: string,
         public StudentId: string,
         public RecaptchaToken: string
     ) {}
     public static get Default(): RegisterViewModel {
-        return new RegisterViewModel('', '', '', '', '');
+        return new RegisterViewModel( '', '', '', '', '', '');
     }
 }
