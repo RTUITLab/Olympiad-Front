328eea49aadaf010d01c9674f8a49a3a2573e38a
ReC <kitanin.ser@mail.ru>
2019-02-14 04:17:34 +0300

    changed property username to Email for correct validation

--- src/app/models/ViewModels/LoginViewModel.ts
+++ src/app/models/ViewModels/LoginViewModel.ts
@@ -1,4 +1,4 @@
 export class LoginViewModel {
-    constructor(public UserName: string, public Password: string) {}
+    constructor(public Email: string, public Password: string) {}
     public static Default(): LoginViewModel {return new LoginViewModel('', ''); }
 }
