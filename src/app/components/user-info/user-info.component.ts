9efad4b851b2dbb47f6f125e764c402296ffcaa3
Unknown <kitanin.ser@mail.ru>
2019-02-17 23:23:41 +0300

    added buttons to route to admin functions and page about site

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -13,16 +13,27 @@ export class UserInfoComponent implements OnInit {
 
   user: User;
 
-  constructor(private router: Router, private usersService: UserStateService) { }
+  constructor(private router: Router,
+     private usersService: UserStateService
+     ) { }
   ngOnInit() {
     this.usersService.currentUserStream.subscribe(U => this.user = U);
   }
   myPage() {
-    this.router.navigate(['personal-page']);
+    this.router.navigate(['user', this.user.id ]);
+  }
+  adminFunctions() {
+    this.router.navigate(['admin-functions']);
+  }
+  about() {
+    this.router.navigate(['about']);
   }
   logout() {
     this.usersService.logOut();
     this.router.navigate(['/login']);
+  }
+  isAdmin(): boolean {
+    return this.usersService.IsAdmin();
   }
 
 }
