2af1939a0e53548d022f0c7cb819375eb738d990
ivlaptev <ivlaptev13@ya.ru>
2021-11-02 00:03:05 +0300

    Added user name to header

--- src/app/components/admin-header/admin-header.componenet.ts
+++ src/app/components/admin-header/admin-header.componenet.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
+import {UserStateService} from '../../services/Users/user-state.service';
 
 @Component({
   selector: 'app-admin-header',
@@ -8,7 +9,7 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./admin-header.component.scss']
 })
 export class AdminHeaderComponent {
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public userService: UserStateService) {
     iconRegistry.addSvgIcon(
       'profile',
       sanitizer.bypassSecurityTrustResourceUrl('../../../assets/icon/profile.svg'));
