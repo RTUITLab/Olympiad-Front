ed12087f49ac6f2ab226262c0f7010505da4bf0f
IvLaptev <ivlaptev13@ya.ru>
2022-01-09 11:43:50 +0300

    Added redirect to login with redirect parameter
    redsolved #70

--- src/app/services/Users/user-state.service.ts
+++ src/app/services/Users/user-state.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable, Subscriber } from 'rxjs';
 import { User } from '../../models/Users/User';
 import { LoginViewModel } from '../../models/Login/LoginViewModel';
